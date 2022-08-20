eacfcc60145acf9ca82dc95231d46c38c4ee55ed
mohammaderfankh <79629565+mohammaderfankh@users.noreply.github.com>
2022-08-20 18:50:04 +0430

    products api

---
+++ js/products.js
@@ -0,0 +1,38 @@
+export const productsData = [
+    {
+        id: 1,
+        title: " تخت خواب دونفره",
+        price: "5 ميليون تومان",
+        imageUrl: "../assets/images/product-1.jpeg",
+    },
+    {
+        id: 2,
+        title: " تخت خواب دونفره",
+        price: "10 ميليون تومان",
+        imageUrl: "../assets/images/product-2.jpeg",
+    },
+    {
+        id: 3,
+        title: " تخت خواب دونفره",
+        price: "15 ميليون تومان",
+        imageUrl: "../assets/images/product-3.jpeg",
+    },
+    {
+        id: 4,
+        title: " تخت خواب دونفره",
+        price: "20 ميليون تومان",
+        imageUrl: "../assets/images/product-4.jpeg",
+    },
+    {
+        id: 5,
+        title: " يخچال سايد باي سايد",
+        price: "25 ميليون تومان",
+        imageUrl: "../assets/images/product-5.jpeg",
+    },
+    {
+        id: 6,
+        title: " مبل تک نفره",
+        price: "5 ميليون تومان",
+        imageUrl: "../assets/images/product-6.jpeg",
+    },
+];
