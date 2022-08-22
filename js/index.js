9786d36d85edf3cb6dbc602257724b950908e2c0
mohammaderfankh <79629565+mohammaderfankh@users.noreply.github.com>
2022-08-22 17:27:04 +0430

    add to cart and storage product

--- js/index.js
+++ js/index.js
@@ -1,10 +1,11 @@
 const cartBtn = document.querySelector(".cart-btn");
-const cart = document.querySelector(".cart");
+const cartModal = document.querySelector(".cart");
 const backDrop = document.querySelector(".backdrop");
 const confirm = document.querySelector(".cart-item-confirm");
 const productsDOM = document.querySelector(".product-center");
 
 import { productsData } from "./products.js";
+let cart = [];
 
 class Products {
     getProduct() {
@@ -30,11 +31,42 @@ class UI {
         });
         productsDOM.innerHTML = result;
     }
+    getAddToCartBtns() {
+        const addToCartBtns = document.querySelectorAll(".add-to-cart");
+        const buttons = [...addToCartBtns];
+        buttons.forEach((btn) => {
+            const id = btn.dataset.id;
+            const isInCart = cart.find((p) => p.id === id);
+            if (isInCart) {
+                btn.innerText = "In Cart";
+                btn.disabled = true;
+            }
+            btn.addEventListener("click", (e) => {
+                e.target.innerText = "In Cart";
+                e.target.disabled = true;
+                // get product from products
+                const addedProduct = Storage.getProduct(id);
+                // add to cart
+                cart = [...cart, { ...addedProduct, quantity: 1 }];
+                // save cart t o localStorage
+                Storage.saveCart(cart);
+            });
+        });
+    }
 }
 
 class Storage {
     static saveProducts(products) {
         localStorage.setItem("products", JSON.stringify(products));
+    }
+
+    static getProduct(id) {
+        const _product = JSON.parse(localStorage.getItem("products"));
+        return _product.find((p) => p.id === parseInt(id));
+    }
+
+    static saveCart(cart) {
+        localStorage.setItem("cart", JSON.stringify(cart));
     }
 }
 
@@ -43,6 +75,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const productsData = products.getProduct();
     const ui = new UI();
     ui.displayProducts(productsData);
+    ui.getAddToCartBtns();
     Storage.saveProducts(productsData);
 });
 //  Modal
@@ -52,12 +85,12 @@ backDrop.addEventListener("click", closeModal);
 
 function showModal() {
     backDrop.style.display = "block";
-    cart.style.opacity = "1";
-    cart.style.top = "20%";
+    cartModal.style.opacity = "1";
+    cartModal.style.top = "20%";
 }
 
 function closeModal() {
     backDrop.style.display = "none";
-    cart.style.opacity = "0";
-    cart.style.top = "-100%";
+    cartModal.style.opacity = "0";
+    cartModal.style.top = "-100%";
 }
